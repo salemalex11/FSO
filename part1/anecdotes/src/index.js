90d9598a697cc612357cc7d1245516510c293e9d
Alex Lamouri <salemalex@hotmail.com>
2020-10-10 02:10:07 -0400

    Update index.js

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => <h1> {props.text} </h1>
+const Button = (props) => <button onClick={props.handleClick}> {props.text} </button>
+const Anecdote = (props) => {
+  return(
+    <div> 
+      <div> {props.text} </div>
+      <div> has {props.votes} votes </div>
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(6).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+
+    const mostVotesIndex = copy.findIndex(k => k === Math.max(...copy))
+    setMostVotes(mostVotesIndex)
+  }
+
+  const next = () => setSelected(Math.floor(Math.random() * 6))
+
+  return (
+    <div>
+      <Header text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+      <Button handleClick={vote} text="vote" />
+      <Button handleClick={next} text="next anecdote" />
+      <Header text="Anecdote with most votes" />
+      <Anecdote text={anecdotes[mostVotes]} votes={votes[mostVotes]} />
+    </div>
+  )
+}
+
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -9,39 +48,6 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
-const Header = (props) => <h1> {props.text} </h1>
-const Button = (props) => <button onClick={props.handleClick}> {props.text} </button>
-const Anecdote = (props) => <p> {props.anecdotes[props.selected]} </p>
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(6).fill(0))
-  const [mostVoted, setMostVoted] = useState(0)
-
-  const voteSelected = () => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-
-    const mostVotes = Math.max(...copy)
-    const mostVotesIndex = copy.findIndex(k => k === mostVotes)
-    setMostVoted(mostVotesIndex)
-  }
-
-  const selectRandom = () => setSelected(Math.floor(Math.random() * 6))
-
-  return (
-    <div>
-      <Header text="Anecdote of the day" />
-      <Anecdote anecdotes={anecdotes} selected={selected} />
-      <Button handleClick={voteSelected} text="vote" />
-      <Button handleClick={selectRandom} text="next anecdote" />
-      <Header text="Anecdote with most votes" />
-      <Anecdote anecdotes={anecdotes} selected={mostVoted} />
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
